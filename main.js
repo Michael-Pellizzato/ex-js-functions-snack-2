526b7d988caee147563ace793b0384e5cf2db478
Michael-Pellizzato <pellizzato.michael97@gmail.com>
2025-04-29 13:04:37 +0200

    snack 3

--- main.js
+++ main.js
@@ -11,3 +11,12 @@ Crea una arrow function che calcola il quadrato di un numero:
 -Definisci una funzione chiamata quadrato che accetta un numero e restituisce il suo quadrato in una sola riga.*/
 
 const quadrato = (a) => a * a;
+
+/*🏆 Snack 3
+Crea una funzione eseguiOperazione:
+-Definisci una funzione eseguiOperazione che accetta tre parametri: due numeri e una funzione operatore (callback). La funzione deve eseguire l'operazione fornita sui due numeri.*/
+
+const sottrazione = (a, b) => a - b;
+const divisione = (a, b) => a / b;
+
+const eseguiOperazione = (a, b, operazione) => operazione(a, b);
