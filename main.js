81f1aca0d95d50bdf6bd18ac9d5706074aa185d5
Michael-Pellizzato <pellizzato.michael97@gmail.com>
2025-04-29 12:56:38 +0200

    snack 3

--- main.js
+++ main.js
@@ -4,12 +4,10 @@ Crea una funzione che somma due numeri:
 -Poi, definisci la stessa funzione somma ma come funzione anonima assegnata a una variabile
 -Quindi, riscrivi la funzione somma con la sintassi delle arrow functions.*/
 
-function somma(a, b) {
-  return a + b;
-}
-
-const sommanumeri = function (a, b) {
-  return a + b;
-};
-
 const sommanumeri2 = (a, b) => a + b;
+
+/*🏆 Snack 2
+Crea una arrow function che calcola il quadrato di un numero:
+-Definisci una funzione chiamata quadrato che accetta un numero e restituisce il suo quadrato in una sola riga.*/
+
+const quadrato = (a) => a * a;
